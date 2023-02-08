aa8be91e4fa3e729cbffb9096cbe4a8aeb9a2621
wantoone <wantoone777@gmail.com>
2023-02-08 23:11:19 +0300

    init commit

---
+++ 2_my/1_select.py
@@ -0,0 +1,33 @@
+import socket
+
+
+# domain:5000
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 5001))
+server_socket.listen()
+
+
+def accept_connection(server_socket):
+	while True:
+		client_socket, addr = server_socket.accept()
+		print('Connection from', addr)
+		send_message(client_socket)
+
+
+def send_message(client_socket):
+	while True:
+		request = client_socket.recv(4096)
+
+		if not request:
+			break
+		else:
+			response = 'Hello world\n'.encode()
+			client_socket.send(response)
+	print('Outside inner while loop')
+	client_socket.close()
+
+
+if __name__ == '__main__':
+	accept_connection(server_socket)
