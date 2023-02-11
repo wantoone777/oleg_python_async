51b917e196d2753b5102cbfac95407d3f1e04b50
wantoone <wantoone777@gmail.com>
2023-02-11 21:33:20 +0300

    coroutines

---
+++ 6/5_coroutines.py
@@ -0,0 +1,33 @@
+
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
+
+def subgen():
+    x = 'Ready to accept message'
+    message = yield x
+    print('Subgen received:', message)
+    
+
+
+# @coroutine
+def average():
+    count = 0
+    summ = 0
+    average = None
+    
+    while True:
+        try:
+            x = yield average
+        except StopIteration:
+            print('Done')
+            break
+        else:
+            count += 1
+            summ += x
+            average = round(summ / count, 2)
+    return average
